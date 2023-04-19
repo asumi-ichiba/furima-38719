d067a7aa36adf85926a98b756fb85218529257af
asumi-ichiba <121480112+asumi-ichiba@users.noreply.github.com>
2023-04-19 15:45:49 +0900

    ログイン、ログアウト機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,24 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index] 
+
   def index
-  
+    @items = Item.all
   end
   
+  def create
+    Tweet.create(tweet_params)
+  end
+
+
+
+
+
+  private
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
 end
