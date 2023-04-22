95d3075b1ffc718de5319928dbc16598b48a3913
asumi-ichiba <121480112+asumi-ichiba@users.noreply.github.com>
2023-04-22 21:26:02 +0900

    ActiveHashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :area
+  belongs_to :number
 end
