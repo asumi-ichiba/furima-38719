8a6efae5d93bf15862df19dde3fc047780c08629
asumi-ichiba <121480112+asumi-ichiba@users.noreply.github.com>
2023-04-26 19:57:42 +0900

    イベント発火設定

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,5 +1,12 @@
 window.addEventListener('load', () => {
   const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+  })
   const taxPriceInput = document.getElementById("add-tax-price");
+  priceInput.addEventListener("input", () => {
+  })
   const profitPriceInput = document.getElementById("profit");
+  priceInput.addEventListener("input", () => {
+    console.log("イベント発火");
+  })
 });
