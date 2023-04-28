1d6f17ba66b17a2e1a7e1fda2047b3c82b22defc
asumi-ichiba <121480112+asumi-ichiba@users.noreply.github.com>
2023-04-28 14:52:33 +0900

    Revert "Revert "商品一覧表示機能""
    This reverts commit 5bf3ec64db97e5e65b658a33b61e2a0b7c369c6b.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,7 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index] 
 
   def index
+    @items = Item.all
     #@items = Item.order("created_at DESC")
   end
   
