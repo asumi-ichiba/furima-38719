063e15072e8c1ab3629aa5128b51bd14e08c0dbb
asumi-ichiba <121480112+asumi-ichiba@users.noreply.github.com>
2023-04-29 18:55:20 +0900

    商品情報編集機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, except: [:index, :show] 
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :move_to_index, only: [:edit] 
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,18 +21,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
-    
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to root_path
+      redirect_to item_params
     else
       render :edit
     end
@@ -43,7 +40,6 @@ class ItemsController < ApplicationController
   private
 
   def move_to_index
-    @item = Item.find(params[:id])
     if @item.user_id != current_user.id
       redirect_to action: :index
     end
@@ -51,5 +47,9 @@ class ItemsController < ApplicationController
   
   def item_params
     params.require(:item).permit(:image, :name, :content, :category_id, :condition_id, :postage_id, :area_id, :number_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
