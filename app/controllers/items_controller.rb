dfb2b35dd1c94bc206d4fd62744fe1c92ac7fb9b
asumi-ichiba <121480112+asumi-ichiba@users.noreply.github.com>
2023-04-29 16:27:46 +0900

    Revert "Revert "商品情報編集機能の実装""
    This reverts commit 8324b5d67eecf49aa9b38a47e77f514b9c6f0fbc.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_index, except: [:index, :show] 
 
   def index
@@ -19,10 +19,26 @@ class ItemsController < ApplicationController
     end
   end
 
-   def show
+  def show
     @item = Item.find(params[:id])
-    @categories = Category.where(id: 2..11)
-   end
+    
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user_id != current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
 
 
 
