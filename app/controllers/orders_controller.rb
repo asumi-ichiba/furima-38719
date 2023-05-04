09bde271e1dcdfd359c6ff8a93b64ea6aef4b233
asumi-ichiba <121480112+asumi-ichiba@users.noreply.github.com>
2023-05-04 22:05:13 +0900

    決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,12 +4,15 @@ class OrdersController < ApplicationController
 
   def index
     @order_departs = OrderDeparts.new
+    @item = Item.find(params[:item_id])
   end
 
 
   def create
-    @order_departs = OrderDeparts.new(order_params)
+    @item = Item.find(params[:item_id])
+    @order_departs = OrderDeparts.new(order_params) 
     if @order_departs.valid?
+      pay_item
       @order_departs.save
       redirect_to root_path
     else
@@ -21,6 +24,15 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_departs).permit(:post_cord, :area_id, :municipalities,:address, :building, :telephone).merge(user_id: current_user.id)
+    params.require(:order_departs).permit(:post_cord, :area_id, :municipalities, :address, :building, :telephone).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_***********"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: order_params[:price],  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
--- app/models/order_departs.rb
+++ app/models/order_departs.rb
@@ -1,7 +1,6 @@
 class OrderDeparts
   include ActiveModel::Model
-  attr_accessor :user_id, :item_id, :post_cord, :area_id, :municipalities,:address, :building, :telephone, :order_id
-
+  attr_accessor :user_id, :item_id, :post_cord, :area_id, :municipalities,:address, :building, :telephone, :token
   with_options presence: true do
     validates :post_cord,      format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
     validates :area_id,        numericality:  { other_than: 1 }
@@ -10,11 +9,10 @@ class OrderDeparts
     validates :telephone,      format: { with: /\A[0-9]{10,11}\z/ }
     validates :user_id
     validates :item_id
-    validates :order_id
   end
   
   def save
     ordre = Order.create(user_id: user_id, item_id: item_id)
-    departs = Departes.create(post_cord: post_cord , area_id: area_id, municipalities: municipalities, address: address, building: building, telephon: telephon, order_id: order_id)
+    departs = Depart.create(post_cord: post_cord , area_id: area_id, municipalities: municipalities, address: address, building: building, telephone: telephone, order_id: ordre.id)
   end
 end
